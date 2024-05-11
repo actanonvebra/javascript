74a18a557decd39bb672ff337bd0d86978ca9ce0
İbrahim Serhat Bulut <96337247+actanonvebra@users.noreply.github.com>
2024-05-11 20:46:17 +0300

    Add files via upload

---
+++ 0x1-intro/exercises.js
@@ -0,0 +1,5 @@
+// single line comment
+
+// welcome to 30 days of javascript
+
+/*multiple comment lines*/
---
+++ 0x1-intro/introduction.js
@@ -0,0 +1,14 @@
+// console.log('bu yazı introduction.js ten geldi')
+
+
+// var, tarihe karışmış.
+// let, akışta değeri değişebilecek olan değişkenler.
+// const, akış içerisinde her zaman sabit değere sahip olacak değişkenler.
+
+let youtubeChannel='proto'
+let channelAge=11
+let isChannelStillActive=true
+let secondChannel=null
+let channelMoney=undefined
+
+console.log(youtubeChannel, channelAge, isChannelStillActive, secondChannel, channelMoney)
